0842abfea088d6f15e2138c0512a8f66e3edd783
DuyCoder <duynv.it.052@gmail.com>
2025-06-16 14:43:35 +0700

    learn c sharp

---
+++ LearnCSharp/LearnCSharp/3_event/EventStructure.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace LearnCSharp._3_event
+{
+    public class EventStructure
+    {
+        class DuLieuNhap : EventArgs
+        {
+            public int Data { set; get; }
+            public DuLieuNhap(int x) => Data = x;
+        }
+
+        // Publisher
+        class UserInput
+        {
+            // delegate void KIEU (object ? sender, EventArgs args)
+            public event EventHandler SuKienNhapSo;
+
+            public void Input()
+            {
+                do
+                {
+                    Console.Write("Nhap vao so nguyen: ");
+                    string s = Console.ReadLine();
+                    int i = Int32.Parse(s);
+                    SuKienNhapSo?.Invoke(this, new DuLieuNhap(i));
+                } while (true);
+            }
+        }
+
+        class TinhCan
+        {
+            public void Sub(UserInput input)
+            {
+                input.SuKienNhapSo += Can;
+            }
+
+            // delegate void KIEU (object ? sender, EventArgs args)
+            public void Can(object sender, EventArgs e)
+            {
+                DuLieuNhap duLieu = (DuLieuNhap)e;
+                int i = duLieu.Data;
+                Console.WriteLine($"Can bac 2 cua {i} la {Math.Sqrt(i)} ");
+            }
+        }
+
+        class TinhBinhPhuong
+        {
+            public void Sub(UserInput input)
+            {
+                input.SuKienNhapSo += BinhPhuong;
+            }
+
+            public void BinhPhuong(object sender, EventArgs e)
+            {
+                DuLieuNhap duLieu = (DuLieuNhap)e;
+                int i = duLieu.Data;
+                Console.WriteLine($"Binh phuong cua {i} la {i * i} ");
+            }
+        }
+
+        // public static void Main(string[] args)
+        // {
+        //     UserInput input = new UserInput();
+        //     input.SuKienNhapSo += (sender, e) =>
+        //     {
+        //         DuLieuNhap duLieu = (DuLieuNhap)e;
+        //         Console.WriteLine("Ban vua nhap so : " + duLieu.Data);
+        //     };
+        //
+        //     TinhCan tinhCan = new TinhCan();
+        //     tinhCan.Sub(input);
+        //     TinhBinhPhuong tinhBinhPhuong = new TinhBinhPhuong();
+        //     tinhBinhPhuong.Sub(input);
+        //     
+        //     input.Input();
+        // }
+    }
+}
---
+++ LearnCSharp/LearnCSharp/4_ExtensionMethod/ExtensionMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using MyLib;
+
+namespace LearnCSharp._4_ExtensionMethod
+{
+    static class Abc
+    {
+        // this ở đây la chi dinh mo rong cho lop nao
+        // this string la mo rong cho lop string, this double mo rong cho double....
+        public static void Print(this string s, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(s);
+        }
+    }
+
+    public class ExtensionMethod
+    {
+        // public static void Main(string[] args)
+        // {
+        //     "Xin".Print(ConsoleColor.Cyan);
+        //     "Chao".Print(ConsoleColor.DarkRed);
+        //     "Cac".Print(ConsoleColor.DarkBlue);
+        //     "Ban".Print(ConsoleColor.DarkRed);
+        //
+        //     double a = 2.5;
+        //     Console.WriteLine(a.BinhPhuong());
+        //     Console.WriteLine(a.CanBacHai());
+        //     Console.WriteLine(a.Sin());
+        //     Console.WriteLine(a.Cos());
+        //
+        // }
+    }
+}
