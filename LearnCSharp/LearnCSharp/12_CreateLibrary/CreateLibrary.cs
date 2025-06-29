6cdff856c27c8b256d644a996b30a03ecb43adfd
DuyCoder <duynv.it.052@gmail.com>
2025-06-29 18:42:03 +0700

    create_library

---
+++ LearnCSharp/LearnCSharp/12_CreateLibrary/CreateLibrary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnCSharp._12_CreateLibrary
+{
+    public class CreateLibrary
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
--- LearnCSharp/LearnCSharp/Program.cs
+++ LearnCSharp/LearnCSharp/Program.cs
@@ -1,9 +1,12 @@
+using System;
+
 namespace LearnCSharp
 {
     internal class Program
     {
         // public static void Main(string[] args)
         // {
+        //     Console.WriteLine("Hello");
         // }
     }
 }
